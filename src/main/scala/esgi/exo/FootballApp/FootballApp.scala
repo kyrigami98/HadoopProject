f33603cff6a9f4937a62252126d68173de8314dc
Kyriel DOSSOU <Kyrigami@gmail.com>
2020-06-04 19:14:07 +0200

    initialisation des classes et résolution des bugs

---
+++ src/main/scala/esgi/exo/FootballApp/FootballApp.scala
@@ -0,0 +1,19 @@
+package esgi.exo.FootballApp
+
+class FootballApp(
+    var X2: Int,
+    var X4: Int,
+    var X5: Int,
+    var X6: Int,
+    var adversaire: String,
+    var score_france: Int,
+    var score_adversaire: Int,
+    var penalty_france: Int,
+    var penalty_adversaire: Int,
+    var date: String,
+    var year: String,
+    var outcome: String,
+    var no: Int
+    ) {
+
+}
--- src/main/scala/esgi/exo/FootballApp/HelloWorld.scala
+++ src/main/scala/esgi/exo/FootballApp/HelloWorld.scala
@@ -1,7 +1,15 @@
 package esgi.exo.FootballApp
 
+import org.apache.spark.sql.types._
+import org.apache.spark
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.SQLContext
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("Hello World")
+    println("App Foot")
+
+    val FootballApp1 = new FootballApp(2, 32, 32, 32, ",kfk", 32, 32, 32, 32, "ll", "554", "d", 2)
+    println(FootballApp1.X2)  // prints (2, 3)
   }
 }
